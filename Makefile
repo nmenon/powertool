nmenon/powertool

commit b400e2931b7689d1339623e465b09b8be44235f1

Makefile: add ability to build sandbox
Useful to run valgrind to test for memory leaks and debug

Flip side, the build needs ARCH=arm as a parameter to build
but then, that is what we do in kernel as well..

Signed-off-by: Nishanth Menon <nm@ti.com>

Makefile
@@ -42,10 +42,17 @@ CROSS_CC = ${CROSS_COMPILE}gcc
 # Program name definition for ARM GNU Linker.
 CROSS_LD = ${CROSS_COMPILE}gcc
 # Program name definition for ARM GNU Strip
-CROSS_STRIP = ${CROSS_COMPILE}strip
+ifeq (${ARCH}, "arm")
+  CROSS_STRIP = ${CROSS_COMPILE}strip
+else
+  CROSS_STRIP = /bin/true
+endif
 
 # Flags for LD
-CROSS_LFLAGS += --gc-sections --static -lm
+CROSS_LFLAGS += -lm
+ifeq (${ARCH}, "arm")
+  CROSS_LFLAGS += --gc-sections --static
+endif
 
 # Includes for our compiler
 CROSS_CFLAGS += -I ./src -I ./include -I ./lib

commit 4e37ef2bda3e8ed25ce92282063b27504145cfb2

Makefile: fix ifeq typo for build option
Commit b400e2931b7689d1339623e465b09b8be44235f1
(Makefile: add ability to build sandbox)

broke arm builds, put it back in by fixing the ifeq

Signed-off-by: Nishanth Menon <nm@ti.com>

Makefile
@@ -50,7 +50,7 @@ endif
 
 # Flags for LD
 CROSS_LFLAGS += -lm
-ifeq (${ARCH}, "arm")
+ifeq ($(ARCH),arm)
   CROSS_LFLAGS += --gc-sections --static
 endif
 