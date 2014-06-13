49b15096be1ddf43103bb7285a9cbfbc933d542a
Nishanth Menon <nm@ti.com>
2014-06-13 03:44:11 -0500

    proj-types: add some generic includes
    Based in part on Linux kernel code
    https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/
    tag v3.15
    
    Signed-off-by: Nishanth Menon <nm@ti.com>

---
+++ include/proj-types.h
@@ -0,0 +1,77 @@
+/*
+ * Standard type definitions for the project
+ *
+ * Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/
+ *	Nishanth Menon
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __PROJTYPES__
+#define __PROJTYPES__
+
+#include <ctype.h>
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef u8 bool;
+
+enum bool_type {
+	false = 0,
+	true = 1,
+};
+
+#define SAFE_SET(val, mask) (((val) << __ffs(mask)) & (mask))
+
+/* From Linux kernel Kernel tag: v3.15 */
+#define BIT(nr)                 (1UL << (nr))
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]) + __must_be_array(arr))
+
+/**
+ * __ffs - find first bit in word.
+ * @word: The word to search
+ *
+ * Undefined if no bit exists, so code should check against 0 first.
+ */
+static __always_inline unsigned long __ffs(unsigned long word)
+{
+	int num = 0;
+
+#if BITS_PER_LONG == 64
+	if ((word & 0xffffffff) == 0) {
+		num += 32;
+		word >>= 32;
+	}
+#endif
+	if ((word & 0xffff) == 0) {
+		num += 16;
+		word >>= 16;
+	}
+	if ((word & 0xff) == 0) {
+		num += 8;
+		word >>= 8;
+	}
+	if ((word & 0xf) == 0) {
+		num += 4;
+		word >>= 4;
+	}
+	if ((word & 0x3) == 0) {
+		num += 2;
+		word >>= 2;
+	}
+	if ((word & 0x1) == 0)
+		num += 1;
+	return num;
+}
+
+#define __maybe_unused                     __attribute__((unused))
+
+#endif	/* __PROJTYPES__ */
